4e574d38e4d1989b164435a8324d0898ef5604c1
Haruhiro Yoshimoto <hpnpwd@gmail.com>
2015-02-05 12:53:36 +0900

    retrieve @<lat>,<long> in google map's url

--- app/src/main/java/com/example/PebbleKitExample/GetBackPlusActivity.java
+++ app/src/main/java/com/example/PebbleKitExample/GetBackPlusActivity.java
@@ -4,14 +4,18 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.getpebble.android.kit.PebbleKit;
 import com.getpebble.android.kit.util.PebbleDictionary;
+
+import java.lang.Runnable;
 
 import java.util.UUID;
 import java.util.regex.Pattern;
@@ -63,11 +67,34 @@ public class GetBackPlusActivity extends Activity {
       PebbleKit.sendDataToPebble(getApplicationContext(), WATCHAPP_UUID, data);
     }
     private void startWebView(String loadUrl) {
-      WebView webView = new WebView(this);
+      Toast.makeText(getApplicationContext(), "loading url..", Toast.LENGTH_LONG).show();
+      final WebView webView = new WebView(this);
       webView.setWebViewClient(new WebViewClient() {
         @Override
         public void onPageFinished(WebView view, String url) {
           Log.d("GetBackPlus", "onPageFinished: " + url);
+          Handler handler = new Handler();
+          // Google Map Rewrites URL using JavaScript to add marker
+          // http://.../@<lat>,<lng>,<zoom>
+          handler.postDelayed(new Runnable(){
+            @Override
+            public void run(){
+              boolean found = false;
+              if(webView.getUrl() != null){
+                Log.d("GetBackPlus", "after runnable:" + webView.getUrl());
+                Pattern pat = Pattern.compile("@([0-9.]+,[0-9.]+)");
+                Matcher mat = pat.matcher(webView.getUrl());
+                if(mat.find()) {
+                  EditText et = (EditText)findViewById(R.id.latlng);
+                  et.setText(mat.group(1));
+                  found = true;
+                }
+              }
+              if(!found) {
+                Toast.makeText(getApplicationContext(), "latlong not found", Toast.LENGTH_SHORT).show();
+              }
+            }
+          },3000);
         }
       });
       webView.getSettings().setJavaScriptEnabled(true);
